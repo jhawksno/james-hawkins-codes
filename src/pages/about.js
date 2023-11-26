23b225a1d8d521f2f59dcd56c0d8e73e388e7d19
jhawksno <james.hawkins@snolab.ca>
2023-11-26 16:57:20 -0500

    Added Blog Page and components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,20 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <StaticImage
+        alt="James Hawkins Profile Photo"
+        src="../images/SNOLAB_Circle_Profile.png"
+      />
     </Layout>
   )
 }
 
-export const Head = () => <title>About Me</title>
+export const Head = () => <Seo title="About Me" />
 
 export default AboutPage
