0d2919556cfd2dfa93a4e5968424e29ab008701a
jhawksno <james.hawkins@snolab.ca>
2023-11-27 06:41:57 -0500

    Added images to blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
     title: "James Hawkins Codes",
+    description: `Example project for the Gatsby Head API`,
+    twitterUsername: `@gatsbyjs`,
+    image: `/gatsby-icon.png`,
+    siteUrl: "https://www.jameshawkins.codes",
   },
   plugins: [
     "gatsby-plugin-image",
@@ -10,7 +14,9 @@ module.exports = {
       options: {
         name: `blog`,
         path: `${__dirname}/blog`,
-      }
+      },
     },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
   ],
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import { Link, useStaticQuery, graphql } from 'gatsby'
+import * as React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby"
 import {
   container,
   heading,
@@ -7,7 +7,7 @@ import {
   navLinkItem,
   navLinkText,
   siteTitle,
-} from './layout.module.css'
+} from "./layout.module.css"
 
 const Layout = ({ pageTitle, children }) => {
   const data = useStaticQuery(graphql`
